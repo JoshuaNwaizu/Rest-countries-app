72e1f434de3b8346f6ae308af95f086fbe7c93c5
JoshuaNwaizu <joshuachris000@gmail.com>
2024-05-20 18:25:19 +0100

    New changes

--- src/body/ArrowToTop.tsx
+++ src/body/ArrowToTop.tsx
@@ -1,7 +1,7 @@
 // import React from 'react'
 
 const ArrowToTop = () => {
-  return <div className="fixed">ArrowToTo jjkjfj sd oauisdiuh iaus dgp</div>;
+  return <div className="fixed hidden"></div>;
 };
 
 export default ArrowToTop;
